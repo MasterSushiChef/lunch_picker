cbefbb6626e38c3e29dbb4d9d4d0fa67436102d2
Anthony Chang <anthony.chang@utexas.edu>
2020-01-18 23:36:00 -0600

    key

--- lunch_picker/settings.py
+++ lunch_picker/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '265e6cj5zgn(*ha_k)snr)9=mk5j*lsp*u%h(wp%5u_lt86z@5'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
